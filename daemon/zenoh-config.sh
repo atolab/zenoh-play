e3f1cf2b5a0c1015a370dd9cc83adb334abdbc19
Luca Cominardi <luca.cominardi@gmail.com>
2021-11-30 16:38:39 +0100

    Config file fix

--- daemon/zenoh-config.sh
+++ daemon/zenoh-config.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-ZENOH_URL="http://localhost:8000"
-INFLUXDB_URL="http://localhost:8086"
-NAME="daemon"
-PATH="/${NAME}/**"
-
-curl -X PUT -H 'content-type:application/properties' -d "url=${URL}" ${ZENOH_URL}/@/router/local/plugin/storages/backend/influxdb
-curl -X PUT -H 'content-type:application/properties' -d "path_expr=${PATH};db=${NAME};create_db" ${ZENOH_URL}/@/router/local/plugin/storages/backend/influxdb/storage/${NAME}
+curl -X PUT -H 'content-type:application/properties' -d "url=http://localhost:8086" http://localhost:8000/@/router/local/plugin/storages/backend/influxdb
+curl -X PUT -H 'content-type:application/properties' -d "path_expr=/daemon/**;db=daemon;create_db" http://localhost:8000/@/router/local/plugin/storages/backend/influxdb/storage/daemon
