b2be313447c0b498a3ed9e38be69f1fd26e5fe54
gabrik <gabriele.baldoni@gmail.com>
2022-03-10 14:11:23 +0100

    Removed useless zenoh-config.sh
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- daemon/zenoh-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -X PUT -H 'content-type:application/properties' -d "url=http://localhost:8086" http://localhost:8000/@/router/local/plugin/storages/backend/influxdb
-curl -X PUT -H 'content-type:application/properties' -d "path_expr=/paris/**;db=daemon;create_db" http://localhost:8000/@/router/local/plugin/storages/backend/influxdb/storage/daemon
